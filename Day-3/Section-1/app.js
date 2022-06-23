c70d2e86fc4be75d697e76c97df3fec9ca66a178
Harshad1705 <77875603+Harshad1705@users.noreply.github.com>
2022-06-23 14:38:46 +0530

    Update app.js

--- Day-3/Section-1/app.js
+++ Day-3/Section-1/app.js
@@ -8,3 +8,18 @@ console.log(x)
 obj.name = "Acropolis"
 x = obj.name
 console.log(x)
+
+// creating function inside object
+
+function xyz(){
+    console.log("function xyz is called..")
+}
+var obj_2 = {
+    code:0827,
+    name:"Acro",
+    getName:xyz,
+    getName_2:()=>{console.log("called")},
+}
+
+obj_2.getName()
+obj_2.getName_2()
